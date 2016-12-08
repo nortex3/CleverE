8ea8604d83502150346f226626faf32b3dcf8882
MEGuim <miguelguimaraess@hotmail.com>
2016-12-08 05:25:49 +0000

    UserTraffic + JÁ ACEITA METEOROLOGIA EM QUALQUER CIDADE

---
+++ src/Business/UserTraffic.java
@@ -0,0 +1,138 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author Miguel
+ */
+public class UserTraffic {
+    
+    private final String bingMapsKey = "ApanNzfcCujm2CegUMWN20gHr78rAwt6x-hz70tjCJ9dtoBnALS4NSupUIqeO_qv";
+    private final String RestUrlBase = "http://dev.virtualearth.net/REST/V1/Traffic/Incidents";
+    
+    ArrayList<acidente> acidentes;
+    
+    public class acidente {
+        int severidade;
+        String descricao; 
+        
+        public acidente(int severidade, String descricao) {
+            this.severidade = severidade;
+            this.descricao = descricao;
+        }
+
+        public int getSeveridade() {
+            return severidade;
+        }
+
+        public String getDescricao() {
+            return descricao;
+        }
+
+        @Override
+        public String toString() {
+            return "acidente{" + "severidade=" + severidade + ", descricao=" + descricao + '}';
+        }
+    }
+    
+    
+    public UserTraffic (double lat, double lon) throws IOException {
+        JSONObject obj = new JSONObject(this.requestJSON(lat, lon));
+        
+        JSONArray res = obj.getJSONArray("resourceSets").getJSONObject(0).getJSONArray("resources");
+        
+        for (int i = 0; i < res.length(); i++) {
+            
+            acidente ac = new acidente(res.getJSONObject(i).getInt("severity"), res.getJSONObject(i).getString("description"));
+            acidentes.add(ac);
+        }
+        
+    }
+    
+    public ArrayList<acidente> getAcidentes() {
+        return acidentes;
+    }
+     
+    private String getBoundingBox(final double pLatitude, final double pLongitude, final int pDistanceInMeters) {
+
+               final double[] boundingBox = new double[4];
+
+               final double latRadian = Math.toRadians(pLatitude);
+
+               final double degLatKm = 110.574235;
+               final double degLongKm = 110.572833 * Math.cos(latRadian);
+               final double deltaLat = pDistanceInMeters / 1000.0 / degLatKm;
+               final double deltaLong = pDistanceInMeters / 1000.0 / degLongKm;
+
+               final double minLat = pLatitude - deltaLat;
+               final double minLong = pLongitude - deltaLong;
+               final double maxLat = pLatitude + deltaLat;
+               final double maxLong = pLongitude + deltaLong;
+
+               boundingBox[0] = minLat;
+               boundingBox[1] = minLong;
+               boundingBox[2] = maxLat;
+               boundingBox[3] = maxLong;
+               
+               StringBuilder sb = new StringBuilder();
+               sb.append(boundingBox[0]);
+               sb.append(",");
+               sb.append(boundingBox[1]);
+               sb.append(",");
+               sb.append(boundingBox[2]);
+               sb.append(",");
+               sb.append(boundingBox[3]);
+               
+               
+               String ret = boundingBox[0] + "," + boundingBox[1] + "," + boundingBox[2] + "," + boundingBox[3];
+
+               return sb.toString();
+       }
+    
+    private String requestJSON(double lat, double lon) throws MalformedURLException, IOException {
+        URL requestUrl;
+        String url = RestUrlBase;
+        
+        url += "/" + getBoundingBox(lat, lon, 2500);
+        url += "?key=" + bingMapsKey;
+        requestUrl = new URL(url);
+        
+        // Make the web request
+        URLConnection conn = requestUrl.openConnection();
+        InputStream inStream = conn.getInputStream();
+        BufferedReader br = new BufferedReader(new InputStreamReader(inStream));
+        String inputLine, output = "";
+        
+        while((inputLine = br.readLine()) != null)
+        {
+                output += inputLine;
+        }
+        
+        br.close();
+        inStream.close();
+        return output;
+    }
+
+    @Override
+    public String toString() {
+        return "UserTraffic{, acidentes=" + acidentes + '}';
+    }
+    
+    
+}
--- src/Business/UserWeather.java
+++ src/Business/UserWeather.java
@@ -9,8 +9,16 @@ import com.github.fedy2.weather.YahooWeatherService;
 import com.github.fedy2.weather.data.Channel;
 import com.github.fedy2.weather.data.Forecast;
 import com.github.fedy2.weather.data.unit.DegreeUnit;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Iterator;
 import javax.xml.bind.JAXBException;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  *
@@ -39,9 +47,9 @@ public class UserWeather {
         }
     }
     
-    public UserWeather(String woeid, String data) throws JAXBException, IOException {
+    public UserWeather(String data) throws JAXBException, IOException {
         this.service = new YahooWeatherService();
-        this.result = service.getForecast(woeid, DegreeUnit.CELSIUS);
+        this.result = service.getForecast("737514", DegreeUnit.CELSIUS);
         for (Forecast dia : this.result.getItem().getForecasts()) {
             if (data.equals(dia.getDate())) {
                 this.tempMin = dia.getLow();
@@ -50,6 +58,55 @@ public class UserWeather {
                 break;
             }
         }
+    }
+    
+    public UserWeather(String cidade, String data) throws JAXBException, IOException {
+        this.service = new YahooWeatherService();
+        this.result = service.getForecast(getWOEID(cidade), DegreeUnit.CELSIUS);
+        for (Forecast dia : this.result.getItem().getForecasts()) {
+            if (data.equals(dia.getDate())) {
+                this.tempMin = dia.getLow();
+                this.tempMax = dia.getHigh();
+                this.descricao = dia.getText();
+                break;
+            }
+        }
+    }
+    
+    private String getWOEID(String cidade) throws IOException {
+        String url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20geo.places%20where%20text%3D%" + cidade + "%22%20AND%20placeTypeName.code%20%3D%207&format=json&diagnostics=true&callback=";
+        URL requestUrl;
+        requestUrl = new URL(url);
+        
+        // Make the web request
+        URLConnection conn = requestUrl.openConnection();
+        InputStream inStream = conn.getInputStream();
+        BufferedReader br = new BufferedReader(new InputStreamReader(inStream));
+        String inputLine, output = "";
+        
+        while((inputLine = br.readLine()) != null)
+        {
+            output += inputLine;
+        }
+        
+        br.close();
+        inStream.close();
+        JSONObject obj = new JSONObject(output);
+                
+        JSONArray arrRes = obj.getJSONArray("results");
+        Iterator<Object> iterator = arrRes.iterator();
+        StringBuilder sb = new StringBuilder();
+        while (iterator.hasNext()) {
+                sb.append(iterator.next());
+        }
+        JSONObject results = new JSONObject(sb.toString());
+        
+        JSONArray arrRes = obj.getJSONObject("query").getJSONObject("results").getJSONArray("place");
+        System.out.println(arrRes.getJSONObject(0).getInt("woeid"));
+        
+        
+        return output;
+        
     }
 
     public YahooWeatherService getService() {
@@ -76,6 +133,8 @@ public class UserWeather {
     public String toString() {
         return "UserWeather{" + "tempMin=" + tempMin + ", tempMax=" + tempMax + ", descricao=" + descricao + '}';
     }
+
+    
     
     
 }
